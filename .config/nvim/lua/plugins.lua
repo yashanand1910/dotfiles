a26f4c9f7fd27f5ba54579341adea9346bcb22ea
Yash Anand <yashanand1910@gmail.com>
2025-02-19 11:33:49 -0500

    Update config on Wed Feb 19 11:33:49 AM EST 2025

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -208,6 +208,15 @@ local plugins = {
 
 	-- Convenience
 	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && npm install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	},
+	{
 		"smjonas/inc-rename.nvim", --> Incremental rename
 		config = true,
 	},
