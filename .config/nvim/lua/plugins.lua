30e357591c29820798ea44dd7e3395dccf7a14d1
Yash Anand <yashanand1910@gmail.com>
2025-06-03 03:17:19 +0000

    Update plugins nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -207,14 +207,14 @@ local plugins = {
 	},
 
 	-- Convenience
-	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		ft = { "markdown" },
-		build = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-	},
+	-- {
+	-- 	"iamcco/markdown-preview.nvim",
+	-- 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	-- 	ft = { "markdown" },
+	-- 	build = function()
+	-- 		vim.fn["mkdp#util#install"]()
+	-- 	end,
+	-- },
 	{
 		"smjonas/inc-rename.nvim", --> Incremental rename
 		config = true,
