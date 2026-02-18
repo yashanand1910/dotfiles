437e68bd622823cfd8d48d1a3e0f3b474b7a51ae
Yash Anand <yashanand1910@gmail.com>
2026-02-18 16:37:04 -0500

    Update config on Wed Feb 18 04:37:04 PM EST 2026

--- .vimrc
+++ .vimrc
@@ -22,7 +22,7 @@ let g:netrw_banner=0
 let g:netrw_browse_split=4
 let g:netrw_altv=1
 let g:netrw_liststyle=3
-let g:netrw_list_hide=netrw_gitignore#Hide()
+" let g:netrw_list_hide=netrw_gitignore#Hide()
 
 let g:tex_flavor='latex' 
 let g:vimtex_view_method = 'skim' 
--- scripts/bootupdate.sh
+++ scripts/bootupdate.sh
@@ -4,10 +4,28 @@ rm -rf /boot/*old
 cp /boot/vmlinuz* /efi/EFI/Linux/kernel.new.efi
 cp /boot/initramfs* /efi/EFI/Linux/initramfs.new.img
 cp /boot/System.map* /efi/EFI/Linux/System.new.map
+if [ -f /boot/intel-uc.img ]; then
+    cp /boot/intel-uc.img /efi/EFI/Linux/intel-uc.new.img
+fi
+if [ -f /boot/amd-uc.img ]; then
+    cp /boot/amd-uc.img /efi/EFI/Linux/amd-uc.new.img
+fi
 mv /efi/EFI/Linux/kernel.efi /efi/EFI/Linux/kernel.backup.efi
 mv /efi/EFI/Linux/initramfs.img /efi/EFI/Linux/initramfs.backup.img
 mv /efi/EFI/Linux/System.map /efi/EFI/Linux/System.backup.map
 mv /efi/EFI/Linux/kernel.new.efi /efi/EFI/Linux/kernel.efi
 mv /efi/EFI/Linux/initramfs.new.img /efi/EFI/Linux/initramfs.img
 mv /efi/EFI/Linux/System.new.map /efi/EFI/Linux/System.map
+if [ -f /efi/EFI/Linux/intel-uc.new.img ]; then
+    if [ -f /efi/EFI/Linux/intel-uc.img ]; then
+        mv /efi/EFI/Linux/intel-uc.img /efi/EFI/Linux/intel-uc.backup.img
+    fi
+    mv /efi/EFI/Linux/intel-uc.new.img /efi/EFI/Linux/intel-uc.img
+fi
+if [ -f /efi/EFI/Linux/amd-uc.new.img ]; then
+    if [ -f /efi/EFI/Linux/amd-uc.img ]; then
+        mv /efi/EFI/Linux/amd-uc.img /efi/EFI/Linux/amd-uc.backup.img
+    fi
+    mv /efi/EFI/Linux/amd-uc.new.img /efi/EFI/Linux/amd-uc.img
+fi
 #sbsign /efi/EFI/Linux/kernel.efi --cert /certs/kernel.pem --key /certs/kernel.pem --out /efi/EFI/Linux/kernel.efi
